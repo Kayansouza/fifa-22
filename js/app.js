71b8d5ef434ce9ebcbfcfb6f4b323a84dfa74c07
richard123kayan <rick.kayan@gmail.com>
2021-11-05 17:52:03 -0300

    meu primeiro site

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const loadProducts = (produtos, iddivparent) ={
+    const parentDiv = document.querySelector (productld)
+    produtos.array.forEach(element => {
+        
+    });
+
+    
+    
+        
+   
+    
+                parentDiv.insertAdjacentHTMl('beforeend, html')
+    }
+
+    loadproducts(produtos,#product-div)
---
+++ js/produto.js
@@ -0,0 +1,191 @@
+const produtos = [
+    {
+        id: 1,
+        Image: 'https://picsum.photos/200'
+        title: 'Produto 01'
+        description: 'descriçao de produto 1'
+        value: 100,00
+    }
+]
+
+id 2,[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 02'
+        description 'descriçao de produto 02'
+        value: 100,00
+    }
+]
+
+id 3,[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 03'
+        description 'descriçao de produto 03'
+        value: 100,00
+    }
+]
+id 4,[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 04'
+        description 'descriçao de produto 04'
+        value:100,00
+    }
+]
+
+id 5,[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 05'
+        description 'descriçao de produto 05'
+        value: 100,00
+
+    }
+]
+
+id 6,[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 06 '
+        description 'descriçao de produto'
+        value: 100,00
+    }
+]
+
+id 7,[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 07'
+        description 'descriçao de produto'
+        value:100,00
+    }
+]
+
+id 8,[
+    {
+        Image:'https://picsum.photos/200'
+         title 'produto 08'
+         description 'descriçao de produto'
+         value: 100,00
+    }
+]
+
+id 9,[
+    {Image:'https://picsum.photos/200'
+        title 'produto 9'
+        description 'descriçao de produto'
+        value: 100,00
+    }
+]
+
+id 10, [
+    Image:'https://picsum.photos/200'
+    title 'produto 10'
+    description 'descriçao de produto'
+    value:100,00
+
+]
+
+id 11, [ {
+    Image:'https://picsum.photos/200'
+    title 'produto 11'
+    description 'descriaçao de produto'
+    value:100,00
+}
+]
+id 12, [
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 12'
+        description 'descriçao de produto'
+        value:100,00
+    }
+]
+id 13,[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 13'
+        description 'descriçao de produto'
+        value:100,00
+    }
+]
+
+id 14,[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 14'
+        description 'descriçao de produto'
+        value:100,00
+    }
+]
+id: 2015,[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 15'
+        description 'descriçao de produto'
+        value:100,00
+    }
+]
+id: 2016,[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 16'
+        description 'descriçao de produto'
+        value: 100,00
+    }
+]
+
+id: 2017[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 17'
+        description 'descriçao de produto'
+        value:100,00
+    }
+]
+
+id 2018 [
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 18'
+        description 'descriçao de produto'
+        value:100,00
+    }
+]
+
+id 2019[
+    {
+        Image:'https://picsum.photos/200'
+        title 'produto 19'
+        description 'descriçao de produto'
+        value:100,00 
+    }
+]
+
+id 2020 [
+    [
+        Image:'https://picsum.photos/200'
+        title 'produto 20'
+        description 'descriçao de produto'
+        value:100,00
+    ]
+]
+
+id 2021 [
+    {
+        Image:'https://picsum.photos/200'
+        title 'prduto 21'
+        description 'descriçao de produto'
+        value:100,00
+    }
+]
+
+id 2022[
+    {
+        Image: 'https://picsum.photos/200'
+        title 'produto 22'
+        description 'descriçao de produto'
+        value:499,00
+    }
+]
